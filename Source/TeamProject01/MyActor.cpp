ad8fc55e9375a7360662172d67eda4d23ca99f90
TsukinoRen <whitealbum1226@gmail.com>
2026-03-23 17:40:45 +0900

    [Other] 새로운 액터 클래스 추가

---
+++ Source/TeamProject01/MyActor.cpp
@@ -0,0 +1,22 @@
+// MyActor.cpp
+
+
+#include "MyActor.h"
+
+AMyActor::AMyActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
---
+++ Source/TeamProject01/MyActor.h
@@ -0,0 +1,23 @@
+// MyActor.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class TEAMPROJECT01_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AMyActor();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+};
